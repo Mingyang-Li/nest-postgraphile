4a0be877f2dde1b0102c564215db0592832388c1
Mingyang-Li <mingyangli118@gmail.com>
2022-09-20 12:22:17 +1200

    chore: upgraded ts + minimised postgraphile module

--- src/postgraphile/postgraphile.middleware.ts
+++ src/postgraphile/postgraphile.middleware.ts
@@ -1,46 +1,5 @@
 import { postgraphile, PostGraphileOptions } from 'postgraphile';
 
-const options: PostGraphileOptions = {
-  ownerConnectionString: process.env.DATABASE_URL,
-  simpleCollections: 'both',
-  pgDefaultRole: 'app_user',
-  subscriptions: true,
-  dynamicJson: true,
-  watchPg: true,
-  ignoreRBAC: false,
-  extendedErrors: [
-    'severity',
-    'code',
-    'detail',
-    'hint',
-    'position',
-    'internalPosition',
-    'internalQuery',
-    'where',
-    'schema',
-    'table',
-    'column',
-    'dataType',
-    'constraint',
-    'file',
-    'line',
-    'routine',
-  ],
-  showErrorStack: 'json',
-  enhanceGraphiql: true,
-  graphiql: true,
-  allowExplain() {
-    // TODO: customise condition!
-    return true;
-  },
-  legacyRelations: 'omit',
-  enableQueryBatching: true,
-  enableCors: true,
-  appendPlugins: [require('@graphile-contrib/pg-simplify-inflector')],
-};
+const options: PostGraphileOptions = {};
 
-export const middleware = postgraphile(
-  process.env.DATABASE_URL,
-  process.env.KEDO_DB_PUBLIC_SCHEMA,
-  options,
-);
+export const middleware = postgraphile(process.env.DATABASE_URL ?? '', options);
--- src/postgraphile/postgraphile.module.ts
+++ src/postgraphile/postgraphile.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PostGraphileController } from './postgraphile.controller';
-import { PostGraphileService } from './postgraphile.service';
 
 @Module({
   controllers: [PostGraphileController],
-  providers: [PostGraphileService],
+  providers: [],
 })
 export class PostgraphileModule {}
